445bb128666bd345e7a1faf2bb76ae31a8de98fc
Pelle <per.franck@gmail.com>
2022-01-24 10:27:54 -0800

    Update Main.groovy
    call the API to obtain programming points for the user

--- src/Main.groovy
+++ src/Main.groovy
@@ -1,8 +1,7 @@
 import Square
 
 def name = "Matthew"
-int programmingPoints = 10
 
 println "Hello ${name}"
-println "${name} has at least ${programmingPoints} programming points."
+println "${name} has at least ${GetUserProgrammingPoints(name)} programming points."
 println "${programmingPoints} squared is ${Square.square(programmingPoints)}"
