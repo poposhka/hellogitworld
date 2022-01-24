dece2d31009a15799c2100b624ce68873fe96eac
Pelle <per.franck@gmail.com>
2022-01-24 11:02:49 -0800

    Update Main.groovy
    changed text output to the more cheerful !

--- src/Main.groovy
+++ src/Main.groovy
@@ -3,5 +3,5 @@ import Square
 def name = "Matthew"
 
 println "Hello ${name}"
-println "${name} has at least ${GetUserProgrammingPoints(name)} programming points."
+println "${name} has at least ${GetUserProgrammingPoints(name)} programming points!"
 println "${programmingPoints} squared is ${Square.square(programmingPoints)}"
